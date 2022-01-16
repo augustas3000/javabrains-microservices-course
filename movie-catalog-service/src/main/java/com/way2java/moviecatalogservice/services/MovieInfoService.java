11bcf7681795023c203bd30dbd5842080b21a9f2
augustas3000 <augusto.pasaulis@gmail.com>
2022-01-16 20:16:37 +0200

    add property to enable stream to hystrix dashboard and circuit breaker on method calling movie-info-service

--- movie-catalog-service/src/main/java/com/way2java/moviecatalogservice/services/MovieInfoService.java
+++ movie-catalog-service/src/main/java/com/way2java/moviecatalogservice/services/MovieInfoService.java
@@ -1,5 +1,7 @@
 package com.way2java.moviecatalogservice.services;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.way2java.moviecatalogservice.models.CatalogItem;
 import com.way2java.moviecatalogservice.models.MovieInfo;
 import com.way2java.moviecatalogservice.models.Rating;
@@ -15,7 +17,14 @@ public class MovieInfoService {
         this.restTemplate = restTemplate;
     }
 
-//    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+    @HystrixCommand(
+            fallbackMethod = "getFallbackCatalogItem",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+            })
     public CatalogItem getCatalogItem(Rating rating) {
         MovieInfo movieInfo = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), MovieInfo.class);
         return new CatalogItem(movieInfo.getTitle(), movieInfo.getOverview(), rating.getRating());
