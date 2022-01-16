bbcde39149bc28df83bea02a54f1da4086ca7a31
augustas3000 <augusto.pasaulis@gmail.com>
2022-01-16 21:01:48 +0200

    add comments on bulkhead pattern through hystrix annotations

--- movie-catalog-service/src/main/java/com/way2java/moviecatalogservice/services/MovieInfoService.java
+++ movie-catalog-service/src/main/java/com/way2java/moviecatalogservice/services/MovieInfoService.java
@@ -17,6 +17,20 @@ public class MovieInfoService {
         this.restTemplate = restTemplate;
     }
 
+//    BULKHEAD PATTERN
+//    @HystrixCommand(
+//            fallbackMethod = "getFallbackCatalogItem",
+//            threadPoolKey = "movieInfoPool",
+//            threadPoolProperties = {
+//                    @HystrixProperty(name = "coreSize", value = "20"),
+//                    @HystrixProperty(name = "maxQueueSize", value = "10")
+//            },
+//            commandProperties = {
+//                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+//                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+//                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+//                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+//            })
     @HystrixCommand(
             fallbackMethod = "getFallbackCatalogItem",
             commandProperties = {
