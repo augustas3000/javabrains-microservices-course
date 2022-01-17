f462320e553189ecc6bcd59cf7e4b3879ed040bc
augustas3000 <augusto.pasaulis@gmail.com>
2022-01-17 07:21:11 +0200

    list and map tricks with @value

--- movie-catalog-service/src/main/java/com/way2java/moviecatalogservice/resources/GreetingsController.java
+++ movie-catalog-service/src/main/java/com/way2java/moviecatalogservice/resources/GreetingsController.java
@@ -4,17 +4,40 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class GreetingsController {
 
-    @Value("${my.greeting}")
+    @Value("${my.greeting: default value}")
     private String greetingMessage;
+
+    @Value("some static message")
+    private String staticMessage;
 
     @Value("${app.description}")
     private String descMessage;
 
+    @Value("${my.list.values}")
+    private List<String> listValues;
+
+//    should work but doesnt? investigate
+//    @Value("#{$dbValues}}")
+//    private Map<String, String> dbValues;
+
     @GetMapping("/greeting")
     public String greeting() {
-        return descMessage;
+        return (greetingMessage + " " + staticMessage);
     }
+
+    @GetMapping("/list-values")
+    public List<String> listOfValuesFromProperties() {
+        return listValues;
+    }
+
+//    @GetMapping("/list-db-values")
+//    public Map<String, String> listOfDbValuesFromProperties() {
+//        return dbValues;
+//    }
 }
